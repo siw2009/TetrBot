f447ab41a60da9bf0a1c27c3d7ece89b06711950
인우 <siw2009@naver.com>
2025-08-20 08:53:04 +0900

    fixed ScreenShot.get_screen

--- ScreenShot.py
+++ ScreenShot.py
@@ -6,5 +6,5 @@ def get_screen(area: tuple[int]) -> bytes:
     with mss() as sct:
         # monitor = sct.monitors[1]
         sct_img = sct.grab(area)
-        size = sct_img.size
-        rlt = sct_img.rgb
+        # size = sct_img.size
+    return sct_img.rgb
