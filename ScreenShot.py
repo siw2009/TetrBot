ed67ec5b955dc6688bf53beaa9cdab9dc5072194
인우 <siw2009@naver.com>
2025-08-20 09:12:06 +0900

    basic implementations

--- ScreenShot.py
+++ ScreenShot.py
@@ -2,7 +2,13 @@ from mss import mss
 
 
 
-def get_screen(area: tuple[int]) -> bytes:
+def get_screen(area: dict) -> bytes:
+    '''
+    area parameter must be a dictionary
+    **top, left, width, height, mon**
+    where mon specifies which monitor to capture from
+    '''
+
     with mss() as sct:
         # monitor = sct.monitors[1]
         sct_img = sct.grab(area)
--- TetrBot.py
+++ TetrBot.py
@@ -1,1 +1,9 @@
-import ScreenShot
+import ScreenShot as scs
+
+
+
+board_config = [10, 20]
+area_config = {'top': 298, 'left': 789, 'width': 343, 'height': 683}
+block_size = [area_config['width'] / board_config[0], area_config['height'] / board_config[1]]
+area = scs.get_screen(area_config)
+
